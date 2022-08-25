eb981c4494e7376cd7c538bd4bd4bb21f7176c31
Derrick Nyasio <dnyasio2014@gmail.com>
2022-08-26 01:30:08 +0300

    Assignment Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    // your code here
+    let form = document.querySelector("form");
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        handleTodo(e.target.new_task_description.value);
+        form.reset();
+    })
 });
+
+function handleTodo(todo) {
+    console.log(todo);
+    let li = document.createElement('li');
+    let btn = document.createElement('button');
+    btn.textContent = " X "
+    btn.addEventListener("click", handleDelete);
+    li.textContent = `${todo}  `;
+    li.appendChild(btn);
+    document.querySelector('#tasks').appendChild(li);
+}
+
+function handleDelete(e) {
+    e.target.parentNode.remove();
+}
